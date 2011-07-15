1848b9107b61060a6e0a2a324076f665dd866000
takkaw <takkaw@gmail.com>
2011-07-16 08:04:08 +0900

    add autograph event

--- scripts/make_db.rb
+++ scripts/make_db.rb
@@ -200,3 +200,85 @@ yamls.each { |yaml|
   }
 }
 
+# add auto graph event
+write_db( 
+  :day => '16',
+  :room_en => "Junkdo",
+  :room_ja => "ジュンク堂書店",
+  :start => "12:00",
+  :end => "13:30",
+  :title_ja => "[サイン会]
+  『7つの言語 7つの世界』",
+  :title_en => "[Autograph session]
+  『7つの言語 7つの世界』",
+  :speaker_en => "まつもとゆきひろ",
+  :speaker_ja => "まつもとゆきひろ",
+  :desc_en => "",
+  :desc_ja => "",
+  :lang => "Japanese",
+  :speaker_bio_en => "",
+  :speaker_bio_ja => "",
+  :gravatar => ""
+)
+
+write_db( 
+  :day => '17',
+  :room_en => "Junkdo",
+  :room_ja => "ジュンク堂書店",
+  :start => "12:00",
+  :end => "13:30",
+  :title_ja => "[サイン会]
+  『Rails3レシピブック 190の技』（ソフトバンク クリエイティブ)",
+  :title_en => "[Autograph session]
+  『Rails3レシピブック 190の技』（ソフトバンク クリエイティブ)",
+  :speaker_en => "高橋征義, 松田明, 諸橋恭介",
+  :speaker_ja => "高橋征義, 松田明, 諸橋恭介",
+  :desc_en => "",
+  :desc_ja => "",
+  :lang => "Japanese",
+  :speaker_bio_en => "",
+  :speaker_bio_ja => "",
+  :gravatar => ""
+)
+
+write_db( 
+  :day => '17',
+  :room_en => "Junkdo",
+  :room_ja => "ジュンク堂書店",
+  :start => "15:40",
+  :end => "16:10",
+  :title_ja => "[サイン会]
+  『ウェブオペレーション』(オライリー・ジャパン))",
+  :title_en => "[Autograph session]
+  『ウェブオペレーション』(オライリー・ジャパン))",
+  :speaker_en => "角征典,濱崎健吾",
+  :speaker_ja => "角征典,濱崎健吾",
+  :desc_en => "",
+  :desc_ja => "",
+  :lang => "Japanese",
+  :speaker_bio_en => "",
+  :speaker_bio_ja => "",
+  :gravatar => ""
+)
+
+write_db( 
+  :day => '18',
+  :room_en => "Junkdo",
+  :room_ja => "ジュンク堂書店",
+  :start => "12:00",
+  :end => "13:30",
+  :title_ja => "[サイン会]
+  『アジャイルサムライ』(オーム社))",
+  :title_en => "[Autograph session]
+  『アジャイルサムライ』(オーム社))",
+  :speaker_en => "西村直人, 角谷信太郎",
+  :speaker_ja => "西村直人, 角谷信太郎",
+  :desc_en => "",
+  :desc_ja => "",
+  :lang => "Japanese",
+  :speaker_bio_en => "",
+  :speaker_bio_ja => "",
+  :gravatar => ""
+)
+
+
